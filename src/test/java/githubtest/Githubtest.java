eaf23802f48eb95928f9e588bcf3f6179cc6f1a0
karthikjj <sadikjj@gmail.com>
2018-01-06 14:13:26 +0530

    second commit

---
+++ src/test/java/githubtest/Githubtest.java
@@ -0,0 +1,27 @@
+package githubtest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class Githubtest {
+
+@Test
+public void logintest() throws IOException{
+	WebDriver wd = new FirefoxDriver();
+	wd.manage().window().maximize();
+	wd.get("https://www.google.co.in");
+	File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("C:\\Workspace\\githubtest\\Screenshot\\test.png"));
+	
+	
+	}
+
+}
